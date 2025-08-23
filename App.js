76c8e71ae1745cfd5dd0b69570d434e5d9b60c91
RylerVaughn <Ryler.heyman@icloud.com>
2025-08-23 08:56:43 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import MapView from "react-native-maps";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function Map() {
+  const [mapType, mapTypeSetter] = useState("satellite");
+
+  function switchMap(curMapType) {
+    if (curMapType == "satellite") {
+      mapTypeSetter("standard");
+    } else {
+      mapTypeSetter("satellite");
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <MapView
+      mapType={mapType}
+      style={{ width: '100%', height: '100%' }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      >
+        <Button title="Swap" 
+        onPress={() => switchMap(mapType)}
+        style={{width: "20%", height: "10%", color: "white"}}
+        ></Button>
+      </MapView>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Welcome() {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+    </View>
+  )
+}
+ 
+function Navigation() {
+  return (
+      <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Welcome">
+              {() => <Welcome></Welcome>}
+            </Tab.Screen>
+            <Tab.Screen name="Map">
+              {() => <Map></Map>}
+            </Tab.Screen>
+          </Tab.Navigator>
+        </NavigationContainer>
+  )
+}
+
+export default Navigation;
